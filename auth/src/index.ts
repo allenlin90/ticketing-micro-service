43bbdc305c6645f25a2df4f245163a49907d9b62
allenlin <ht016746@gmail.com>
2024-01-24 11:27:47 +0700

    chore: connecting to mongo db on cluster

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -22,7 +24,17 @@ app.get('*', async () => {
 
 app.use(errorHandler);
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`auth service is running on port: ${PORT}`);
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+  } catch (err) {
+    console.error(err);
+  }
+
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`auth service is running on port: ${PORT}`);
+  });
+};
+
+start();
