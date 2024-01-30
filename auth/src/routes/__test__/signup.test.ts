edb2093cf277031a7c01df78c001be14f0d65b4e
allenlin <ht016746@gmail.com>
2024-01-30 09:11:57 +0700

    test: signup flow behavior

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -11,4 +11,59 @@ describe('signup route', () => {
       })
       .expect(201);
   });
+
+  it('returns a 400 with an invalid email', async () => {
+    return request(app)
+      .post('/api/users/signup')
+      .send({
+        email: 'invalid email',
+        password: 'password',
+      })
+      .expect(400);
+  });
+
+  it('returns a 400 with an invalid password', async () => {
+    return request(app)
+      .post('/api/users/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'p',
+      })
+      .expect(400);
+  });
+
+  it('returns a 400 with missing email and password', async () => {
+    await request(app)
+      .post('/api/users/signup')
+      .send({
+        email: 'test@test.com',
+      })
+      .expect(400);
+
+    return request(app)
+      .post('/api/users/signup')
+      .send({
+        password: 'password',
+      })
+      .expect(400);
+  });
+
+  it('disallows duplicate emails', async () => {
+    await request(app)
+      .post('/api/users/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'password',
+      })
+      .expect(201);
+
+    return request(app)
+      .post('/api/users/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'password',
+      })
+      .expect(400);
+  });
+
 });
