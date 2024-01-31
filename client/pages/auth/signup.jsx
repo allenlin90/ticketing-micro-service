7beae4290e04d5184d4ade708aa2c1c77224a778
allenlin <ht016746@gmail.com>
2024-02-01 06:58:19 +0700

    chore: api call to sign up user

--- client/pages/auth/signup.jsx
+++ client/pages/auth/signup.jsx
@@ -1,14 +1,43 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await axios.post('/api/users/signup', {
+      email,
+      password,
+    });
+
+    console.log(response.data);
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className='form-group'>
         <label htmlFor='email'>Email Address</label>
-        <input id='email' type='text' className='form-control' />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          id='email'
+          type='text'
+          className='form-control'
+        />
       </div>
       <div className='form-group'>
         <label htmlFor='password'>Password</label>
-        <input id='password' type='password' className='form-control' />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          id='password'
+          type='password'
+          className='form-control'
+        />
       </div>
       <button className='btn btn-primary'>Sign Up</button>
     </form>
