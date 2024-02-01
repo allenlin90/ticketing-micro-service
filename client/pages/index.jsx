c3d5530d6688739a24452001d0f3b45ef19acf4e
allenlin <ht016746@gmail.com>
2024-02-01 08:01:31 +0700

    chore: inter namespace communication in k8s

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,5 +1,23 @@
-const Landing = () => {
-  return <h1>Landing</h1>;
+import axios from 'axios';
+
+const LandingPage = ({ user }) => {
+  console.log('🚀 ~ LandingPage ~ user:', user);
+  return <h1>LandingPage</h1>;
 };
 
-export default Landing;
+LandingPage.getInitialProps = async () => {
+  const response = await axios
+    .get(
+      'http://ingress-nginx-controller.ingress-nginx/api/users/currentuser',
+      {
+        headers: {
+          host: 'ticketing.dev',
+        },
+      }
+    )
+    .catch((err) => console.log(err));
+
+  return { user: response?.data };
+};
+
+export default LandingPage;
