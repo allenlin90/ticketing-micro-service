9bcb19cffe104695116f4481e56462a786b96952
allenlin <ht016746@gmail.com>
2024-02-01 07:13:46 +0700

    refactor: abstract request to custom hook

--- client/pages/auth/signup.jsx
+++ client/pages/auth/signup.jsx
@@ -1,24 +1,21 @@
 import { useState } from 'react';
 import axios from 'axios';
+import useRequest from '../../hooks/use-request';
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState([]);
+
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: { email, password },
+  });
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    try {
-      const response = await axios.post('/api/users/signup', {
-        email,
-        password,
-      });
-
-      console.log(response.data);
-    } catch (err) {
-      setErrors(err.response.data.errors);
-    }
+    doRequest();
   };
 
   return (
@@ -44,16 +41,7 @@ const SignUp = () => {
           className='form-control'
         />
       </div>
-      {!!errors.length && (
-        <div className='alert alert-danger'>
-          <h4>Oops...</h4>
-          <ul className='my-0'>
-            {errors.map((err) => {
-              return <li key={err.message}>{err.message}</li>;
-            })}
-          </ul>
-        </div>
-      )}
+      {errors}
       <button className='btn btn-primary'>Sign Up</button>
     </form>
   );
