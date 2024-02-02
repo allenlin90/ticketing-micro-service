3a0873c30a53fa887c096ab6c2a50f745837b720
allenlin <ht016746@gmail.com>
2024-02-02 08:59:36 +0700

    chore: manually invoke page level getInitialProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import createClient from '../api/build-client';
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return <div>
+    <h1>Header! {currentUser.email}</h1>
+    <Component {...pageProps} />
+  </div>
+    ;
 };
 
-export default App;
+AppComponent.getInitialProps = async ({ Component, ctx }) => {
+  const client = createClient(ctx);
+  const { data = {} } = await client
+    .get('/api/users/currentuser')
+    .catch((err) => console.log(err));
+
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  return { pageProps, ...data };
+};
+
+export default AppComponent;
