ba1866bf530da4f134702f88883573559169ee73
allenlin <ht016746@gmail.com>
2024-02-02 08:26:40 +0700

    chore: calling different endpoints by runtime

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -2,22 +2,30 @@ import axios from 'axios';
 
 const LandingPage = ({ user }) => {
   console.log('🚀 ~ LandingPage ~ user:', user);
+
   return <h1>LandingPage</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
-  const response = await axios
-    .get(
-      'http://ingress-nginx-controller.ingress-nginx/api/users/currentuser',
-      {
-        headers: {
-          host: 'ticketing.dev',
-        },
-      }
-    )
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    // we are on the server
+    const { data = {} } = await axios
+      .get(
+        'http://ingress-nginx-controller.ingress-nginx/api/users/currentuser',
+        {
+          headers: req.headers,
+        }
+      )
+      .catch((err) => console.log(err));
+
+    return { user: data };
+  }
+
+  const { data = {} } = await axios
+    .get('/api/users/currentuser')
     .catch((err) => console.log(err));
 
-  return { user: response?.data };
+  return { user: data };
 };
 
 export default LandingPage;
