9c6419ee68cc9cbdf7e1066e032c5eef07816036
allenlin <ht016746@gmail.com>
2024-02-03 12:57:01 +0700

    test: generate random userId by mongoose

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -38,7 +38,7 @@ afterAll(async () => {
 global.signin = () => {
   // build a JWT payload { id, email }
   const payload = {
-    id: 'test',
+    id: new mongoose.Types.ObjectId().toHexString(),
     email: 'test@test.com',
   };
 
