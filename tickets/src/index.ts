c8d861eaa36ca3e4548b18b3ad97724a19a9a640
allenlin <ht016746@gmail.com>
2024-02-03 09:35:11 +0700

    chore: init tickets service

---
+++ tickets/src/index.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+import { app } from './app';
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+  } catch (err) {
+    console.error(err);
+  }
+
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`auth service is running on port: ${PORT}`);
+  });
+};
+
+start();
