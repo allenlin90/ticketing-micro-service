188d7c76456abe801964eef5e5e2e328425a2c6c
allenlin <ht016746@gmail.com>
2024-02-19 10:00:51 +0700

    refactor: custom publisher

---
+++ nats-test/src/events/ticket-created-publisher.ts
@@ -0,0 +1,11 @@
+import { Publisher } from './base-publisher';
+import { TicketCreatedEvent } from './ticket-created-event';
+import { Subjects } from './subjects';
+
+export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
+
+  publish(data: TicketCreatedEvent['data']) {
+    return super.publish(data);
+  }
+}
